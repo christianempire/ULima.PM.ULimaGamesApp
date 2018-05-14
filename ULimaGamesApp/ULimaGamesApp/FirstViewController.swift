c472f0d6c1d1e0b6c6534c37eda4480d516b169e
System Administrator <root@Mac-mini.local>
2018-05-14 06:25:15 -0700

    Frst commit, setting everything up

---
+++ ULimaGamesApp/ULimaGamesApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ULimaGamesApp
+//
+//  Created by System Administrator on 5/14/18.
+//  Copyright © 2018 Universidad de Lima. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
